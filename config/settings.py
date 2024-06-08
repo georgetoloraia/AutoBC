58b481830efd2023bcdca11e46dd2d4507bb72f8
georgetoloraia <gtoloraia19@gmail.com>
2024-06-08 11:21:12 +0400

    add: [x] .env for API Keys

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,7 @@ TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 quote_currency = 'USDT'
-initial_investment = 5.0  # Modifi if need
+initial_investment = 5.0  # investment amount | Modifi if need
 rsi_period = 14  # User's RSI period
 commission_rate = 0.001  # 0.1% | Modifi if need
 stop_loss_percentage = 0.05  # 5% stop loss | Modifi if need
