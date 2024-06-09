8c8eb07baa0bfa9ea4d175288ed13391a41dcc16
georgetoloraia <gtoloraia19@gmail.com>
2024-06-09 22:37:07 +0400

    modified indicator rsi from 14 to 1

--- indicators/technical_indicators.py
+++ indicators/technical_indicators.py
@@ -5,7 +5,7 @@ def calculate_indicators(df):
     # df['wma'] = talib.WMA(df['close'], timeperiod=14)
     # df['upper_band'], df['middle_band'], df['lower_band'] = talib.BBANDS(df['close'], timeperiod=20, nbdevup=2, nbdevdn=2)
     # df['trix'] = talib.TRIX(df['close'], timeperiod=15)
-    df['rsi'] = talib.RSI(df['close'], timeperiod=14)
+    df['rsi'] = talib.RSI(df['close'], timeperiod=1)
     # df['macd'], df['macd_signal'], df['macd_hist'] = talib.MACD(df['close'], fastperiod=12, slowperiod=26, signalperiod=9)
     # df['atr'] = talib.ATR(df['high'], df['low'], df['close'], timeperiod=14)
     # df['slowk'], df['slowd'] = talib.STOCH(df['high'], df['low'], df['close'], fastk_period=14, slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0)
